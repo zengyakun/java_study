4664399c89361999974ac59a2ff9e26485ee8a8c
zyk <1580648333@qq.com>
2019-05-27 18:16:21 +0800

    lombok

---
+++ day01/src/main/java/cc/eslink/UserInfo.java
@@ -0,0 +1,18 @@
+package cc.eslink;
+
+import lombok.Builder;
+import lombok.Getter;
+
+/**
+ *@ClassName UserInfo
+ *@Description TODO
+ *@Author zeng.yakun (0178)
+ *@Date 2019/5/27 17:58
+ *@Version 1.0
+ **/
+@Builder(toBuilder = true)
+@Getter
+public class UserInfo {
+    private String name;
+    private String email;
+}
---
+++ day01/src/test/java/Test1.java
@@ -0,0 +1,16 @@
+import cc.eslink.UserInfo;
+import org.junit.Test;
+
+/**
+ *@ClassName Test1
+ *@Description TODO
+ *@Author zeng.yakun (0178)
+ *@Date 2019/5/27 18:04
+ *@Version 1.0
+ **/
+public class Test1 {
+
+    @Test
+    public void test() {
+    }
+}
