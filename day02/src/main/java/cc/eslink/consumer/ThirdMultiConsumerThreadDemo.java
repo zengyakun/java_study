279ea8d1c4980a39f1192b5517d50b68de9c098b
zyk <1580648333@qq.com>
2019-07-18 15:39:18 +0800

    kafka消费者

---
+++ day02/src/main/java/cc/eslink/consumer/ThirdMultiConsumerThreadDemo.java
@@ -0,0 +1,87 @@
+package cc.eslink.consumer;
+
+import com.mysql.jdbc.TimeUtil;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+
+import java.time.Duration;
+import java.util.Collections;
+import java.util.Properties;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+/**
+ *@ClassName ThirdMultiConsumerThreadDemo
+ *@Description TODO
+ *@Author zeng.yakun (0178)
+ *@Date 2019/7/17 17:40
+ *@Version 1.0
+ **/
+public class ThirdMultiConsumerThreadDemo {
+    public static final String brokerList = "server-0:9092";
+    public static final String topic = "topic-demo";
+    public static final String groupId = "group.demo";
+
+    public static Properties initConfig() {
+        Properties props = new Properties();
+        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, brokerList);
+        props.put(ConsumerConfig.GROUP_ID_CONFIG, groupId);
+        props.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, true);
+        return props;
+    }
+
+    public static void main(String[] args) {
+        Properties props = initConfig();
+        KafkaConsumerThread consumerThread = new KafkaConsumerThread(props, topic, Runtime.getRuntime().availableProcessors());
+        consumerThread.start();
+    }
+
+    public static class KafkaConsumerThread extends Thread {
+        private KafkaConsumer<String, String> kafkaConsumer;
+        private ExecutorService executorService;
+        private int threadNumber;
+
+        public KafkaConsumerThread(Properties props, String topic, int threadNumber) {
+            kafkaConsumer = new KafkaConsumer<String, String>(props);
+            kafkaConsumer.subscribe(Collections.singletonList(topic));
+            this.threadNumber = threadNumber;
+            executorService = new ThreadPoolExecutor(threadNumber, threadNumber, 0L, TimeUnit.MILLISECONDS,
+                    new ArrayBlockingQueue<>(1000), new ThreadPoolExecutor.CallerRunsPolicy());
+        }
+
+        @Override
+        public void run() {
+            try {
+                while (true) {
+                    ConsumerRecords<String, String> records = kafkaConsumer.poll(Duration.ofMillis(1000));
+                    if (!records.isEmpty()) {
+                        executorService.submit(new RecordsHandler(records));
+                    }
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            } finally {
+                kafkaConsumer.close();
+            }
+        }
+    }
+
+    public static class RecordsHandler extends Thread {
+        public final ConsumerRecords<String, String> records;
+
+        public RecordsHandler(ConsumerRecords<String, String> records) {
+            this.records = records;
+        }
+
+        @Override
+        public void run() {
+            // todo 处理消息
+        }
+    }
+}
